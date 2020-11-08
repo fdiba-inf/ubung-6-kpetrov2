15bdd5e149e0f1fdbf2912b6c146f2d884a06343
kpetrov2 <konstantin.petrov@fdiba.tu-sofia.bg>
2020-11-08 13:18:36 +0000

    08.11 - no assert

--- src/exercise6/PasswordCheck.java
+++ src/exercise6/PasswordCheck.java
@@ -10,19 +10,19 @@ public class PasswordCheck {
         int counter = 0;
         int numbers = 0;
 
-        assert password.length() > 7;
-        
-        for ( int i = 0 ; i < password.length() ; i++) {
-        int digit = password.charAt(i);
-          if (Character.isLetterOrDigit(digit)) {
-            counter++;
-            if (Character.isDigit(digit)) {
-              numbers++;
+        if (password.length() > 7) { 
+          for ( int i = 0 ; i < password.length() ; i++) {
+          int digit = password.charAt(i);
+            if (Character.isLetterOrDigit(digit)) {
+              counter++;
+              if (Character.isDigit(digit)) {
+                numbers++;
+                }
+              }
             }
+          if (counter == password.length() && numbers > 1 ) {
+            System.out.println("Password valid!");
           }
-        }
-        if (counter == password.length() && numbers > 1 ) {
-          System.out.println("Password valid!");
-        }
+        }  
     }
 }
